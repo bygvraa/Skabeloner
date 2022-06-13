9c4dcec02ca3841129b48a93ed8c492bbf0c51d3
Mikkel <eaamiby@students.eaaa.dk>
2022-06-13 13:35:19 +0200

    Flyttet 'PrintArray' til kun at køre på Tests

--- Sortering/MergeSort/MergeSort.Tests.cs
+++ Sortering/MergeSort/MergeSort.Tests.cs
@@ -9,12 +9,27 @@ namespace MergeSort.Tests
         public void TestMergeSort()
         {
             int[] array = new int[] { 34, 18, 15, 45, 67, 11 };
-            
+
+            Console.WriteLine("Usorteret array:");
+            PrintArray(array);
+
             MergeSort.Sort(array);
 
             CollectionAssert.AreEqual(
                 new int[] { 11, 15, 18, 34, 45, 67 }, 
                 array);
+
+            Console.WriteLine("Sorteret array:");
+            PrintArray(array);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]}");
+            }
+            Console.WriteLine("\n");
         }
     }
 }
--- Sortering/QuickSort/QuickSort.Tests.cs
+++ Sortering/QuickSort/QuickSort.Tests.cs
@@ -10,11 +10,26 @@ namespace QuickSort.Tests
         {
             int[] array = new int[] { 34, 18, 15, 45, 67, 11, 1 };
             
+            Console.WriteLine("Usorteret array:");
+            PrintArray(array);
+
             QuickSort.Sort(array);
 
             CollectionAssert.AreEqual(
                 new int[] { 1, 11, 15, 18, 34, 45, 67 }, 
                 array);
+
+            Console.WriteLine("Sorteret array:");
+            PrintArray(array);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]}");
+            }
+            Console.WriteLine("\n");
         }
     }
 }
