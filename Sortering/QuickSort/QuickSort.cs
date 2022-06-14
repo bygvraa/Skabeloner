3aa7ba6e803e698e43c7c5db7747dd2a7b9961a9
Mikkel <eaamiby@students.eaaa.dk>
2022-06-14 17:55:09 +0200

    Tilføjet flere kommentarer til 'QuickSort'

--- Sortering/QuickSort/QuickSort.cs
+++ Sortering/QuickSort/QuickSort.cs
@@ -2,7 +2,17 @@
 
 /* QuickSort O(n²)
  * Performer i praksis bedre end MergeSort, men worst case er faktisk O(n²).
- * Det er dog sjældent, at man får worst case med en QuickSort.
+   Det er dog sjældent, at man får worst case med en QuickSort.
+
+ * QuickSort-algoritmen følger idéen om "Divide and Conquer", hvor:
+    Divide:   Gør problemet mindre, ved at skære problemet i mindre stykker
+    Conquer:  Løs de mindre stykker af problemet
+    Combine:  Sæt de løste stykker af problemet sammen igen, for at få én samlet løsning
+
+ * QuickSort-algoritmen starter med at vælge et element (kaldet en 'pivot'),
+   der deler arrayet op i to dele, og pivot'en fungerer derved som et samlepunkt mellem de to dele af arrayet.
+ * Idéen er så, at der på den venstre side af 'pivot' samles alle elementer, der har en lavere værdi end pivot'en,
+   og på den højre side samles alle værdierne, der har en højere værdie end pivot'en.
  */
 
 /* GIF
@@ -30,7 +40,7 @@ public static class QuickSort
 
     private static int Partition(int[] array, int low, int high)
     {
-        // Pivot er det laveste tal (altså low)
+        // Vælger pivot til at være det laveste tal (altså low)
         int pivot = array[low];
 
         // To tællevariabler
@@ -43,6 +53,7 @@ public static class QuickSort
             // Hvis værdien i arrayet er lig eller mindre end pivot'en:
             if (array[i] <= pivot)
             {
+                // Gå til næste element på listen
                 i++;
             }
             else if (array[j] > pivot)
@@ -57,7 +68,7 @@ public static class QuickSort
             }
         }
         Swap(array, low, j);
-        return j;                   // index j er vores endelige pivot
+        return j;                   // index 'j' er vores endelige pivot
 
     }
 
